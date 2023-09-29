5bacd9729175fa13a41746f53221b61be73cba80
David Nazariego <dnazariego@gmail.com>
2023-09-29 10:13:20 -0300

    actualizacion

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -2,14 +2,13 @@ import React from 'react'
 import ItemCard from '../ItemCard/ItemCard'
 import './ItemList.scss'
 
-const ItemList = ({ productos, category }) => {
-    const productosFiltrados = productos.filter((producto) => producto.categoria === category);
+const ItemList = ({ productos }) => {
 
     return (
         <div className='Flex'> 
             <div className="container">
                 {
-                    productosFiltrados.map((producto) => (
+                    productos.map((producto) => (
                         <ItemCard key={producto.id} producto={producto}/>
                     ))
                 }
